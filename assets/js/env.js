c0eee1f7960df14e37681c929b0ca2916d9fa3b0
TeamZowasel <austine@zowasel.com>
2023-03-29 13:45:06 +0100

    Changed baseURL

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,14 +1,16 @@
 let localBaseUrl = "http://localhost:3000/api";
+let chrisxBaseUrl = "https://isscunseci.us-east-1.awsapprunner.com/api";
+let developmentBaseUrl = "https://hjh83ppbwi.us-east-1.awsapprunner.com/api";
+let productionBaseUrl = "https://mobileapi.growsel.com/api";
 // https://mobileapi.growsel.com/api
 // https://isscunseci.us-east-1.awsapprunner.com/api
 
 
 
-let liveGlobalBaseUrl = localBaseUrl;
-let liveBaseUrl = localBaseUrl;
+let liveGlobalBaseUrl = productionBaseUrl;
+let liveBaseUrl = productionBaseUrl;
 
-// let liveMobileUrl = "https://hjh83ppbwi.us-east-1.awsapprunner.com/api";
-let liveMobileUrl = localBaseUrl;
+let liveMobileUrl = productionBaseUrl;
 
 let FLW_PUBLIC_KEY="FLWPUBK_TEST-a1b8a6d0b897f10b7332e3af9f902c70-X";
 let FLW_SECRET_KEY="FLWSECK_TEST-0d5cef8e6035065ec995c21244481e32-X";
