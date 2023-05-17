6165c3a06226d769de0a24e9f66f9a2daa0f0f01
TeamZowasel <austine@zowasel.com>
2023-05-17 08:47:11 +0100

    Update on Company profile updating

--- assets/js/env.js
+++ assets/js/env.js
@@ -9,11 +9,11 @@ let socketProductionURL = "https://mobileapi.growsel.com";
 // https://isscunseci.us-east-1.awsapprunner.com/api
 
 
-let socketURL = socketLocalURL;
-let liveGlobalBaseUrl = localBaseUrl;
-let liveBaseUrl = localBaseUrl;
+let socketURL = socketProductionURL;
+let liveGlobalBaseUrl = productionBaseUrl;
+let liveBaseUrl = productionBaseUrl;
 
-let liveMobileUrl = localBaseUrl;
+let liveMobileUrl = productionBaseUrl;
 
 // let FLW_PUBLIC_KEY="FLWPUBK_TEST-a1b8a6d0b897f10b7332e3af9f902c70-X";
 // let FLW_SECRET_KEY="FLWSECK_TEST-0d5cef8e6035065ec995c21244481e32-X";
